c3458bb5dbd65900920e67de9ea6db5ddaf7ae7a
Evaristo Ramalho <evaristo.ra@hotmail.com>
2022-04-25 21:33:04 -0300

    Add Vue Code

---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid>
+      <v-form>
+          <v-file-input
+            label="Selecione as Legendas"
+            prepend-icon="mdi-message-text"
+            append-outer-icon="mdi-send"
+            outlined
+            multiple 
+            chips 
+            v-model="files"
+            @click:append-outer="processSubtitles"
+          />
+      </v-form>
+      <div class="pills">
+          <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
+      </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from './Pill.vue'
+export default {
+    name: 'HomeItem',
+    components: {Pill},
+    data: function () {
+        return {
+            files:[],
+            groupedWords: [
+                {name: 'you', amount: 900},
+                {name: 'he', amount: 853},
+                {name: 'i', amount: 1234},
+            ]
+        }
+    },
+    methods: {
+        processSubtitles(){
+            console.log(this.files)
+        }
+    }
+}
+</script>
+
+<style>
+.pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+</style>
